f8acc94f152125683baab223f9a40dabfeadf868
Manfred <FlatheadV8@users.noreply.github.com>
2014-11-01 16:39:20 +0100

    Update film_bearbeiten.sh
    Fehler behoben

--- film_bearbeiten.sh
+++ film_bearbeiten.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #set -x
-VERSION="v2014081700"
+VERSION="v2014110100"
 
 #------------------------------------------------------------------------------#
 ### Arbeitsweise
@@ -66,8 +66,8 @@ MEINVERZEICHNIS="$(dirname ${0})"
 . ${MEINVERZEICHNIS}/film_bearbeiten_eigenschaften_lesen                        # ist noch nicht fertig
 
 . ${MEINVERZEICHNIS}/film_bearbeiten_crop                                       # crop
-. ${MEINVERZEICHNIS}/film_bearbeiten_standardformat                             # AutoFormat => 4/3 oder 16/9
 . ${MEINVERZEICHNIS}/film_bearbeiten_quadratische_bildpunkte                    # QuadratischePixel => Blu-Ray-Format
+. ${MEINVERZEICHNIS}/film_bearbeiten_standardformat                             # AutoFormat => 4/3 oder 16/9
 
 . ${MEINVERZEICHNIS}/film_bearbeiten_breit_und_hoch                             # wenn noetig, die Breite bzw. Hoehe ermitteln
 . ${MEINVERZEICHNIS}/film_bearbeiten_bluray-kompatible_parameter_ermitteln      # Blu-Ray-Kode
