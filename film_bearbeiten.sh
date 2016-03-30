b735cfa4fb5ba0ef84571910693fbf72166c73ed
Manfred <FlatheadV8@users.noreply.github.com>
2016-03-30 18:27:51 +0200

    Update film_bearbeiten.sh
    Erweiterungen für die beiden neuen Parameter "-mkv" und "-mp4".

--- film_bearbeiten.sh
+++ film_bearbeiten.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #set -x
-VERSION="v2016031200"
+VERSION="v2016032900"
 
 #------------------------------------------------------------------------------#
 ### Arbeitsweise
@@ -33,19 +33,63 @@ VERSION="v2016031200"
 ### AVC-Profil
 PROFILE="high"                          # Standardwert: es wird zur Zeit nur das Profil "high" unterstützt
 
-### Standardwerte für nicht übergebene Parameter
+### Standardwerte fuer nicht uebergebene Parameter
 P_AAC_CODEC="libfaac"                   # leider ist das freie "aac" noch nicht fertig, deshalb verwenden wir erstmal "libfaac"
 TESTSZ="300-330"
 SOLLTONSPUREN="2"
 AS="NEIN"                               # im Standard kein Audio-stretch, zu aktivieren mit "-syncstretch"
 DELAYOTON="0"                           # im Standard kein versetztes einsetzen der Originaltonspur, zu aktivieren mit z.B.: "-doton 1000"
 DELAYTRANSTON="0"                       # im Standard kein versetztes einsetzen der transkodierten Tonspur, zu aktivieren mit z.B.: "-dtton 1000"
-MKVSYNC="JA"                            # im Standard Synchronisation durch den MKV-Container, zu deaktivieren mit: "-syncmkv"
 PPROFIL="avc"                           # im Standard wird ein AVC-Film erzeugt, zu ändern mit z.B.: "-profil asp"
 RAUSCHW="0"                             # im Standard wird keine Rauschunterdrückung aktiviert, zu aktivieren mit z.B.: "-entrauschen 100"
 ANPASSUNG="JA"                          # wichtig fuer die Simple-Profile
+
 VOLLBILDKONVERTIERER="yadif"            # Zeilenentflechtung / Deinterlacing
-VIDEOFILTER_FORMAT="format='yuv444p16le|yuv422p16le|yuv444p|yuv422p|yuv420p'"
+
+#------------------------------------------------------------------------------#
+### ZwischenFormat (alt): FFv1 + PCM16 + MKV
+
+# Dieses Format wurde fuer das zwischenspeichern verwendet, weil hier auf
+# folgende Schwerpunkte wert gelegt wurde:
+# 1. verlusstfreies Video-Format
+# 2. moeglichst verlusstfreies Audio-Format
+# 3. moeglichst universelles Container-Format
+
+ZWISCHENFORMAT_VIDEO_CODEC="ffv1"               # verlusstfreier Codec
+VIDEOFILTER_FORMAT="format='yuv444p|yuv444p16le|yuv422p|yuv422p16le|yuv420p'"
+#ZWISCHENFORMAT_AUDIO_CODEC="pcm_s16le"         # CD-Qualitaet
+ZWISCHENFORMAT_AUDIO_CODEC="pcm_f64le"          # beste PCM-Qualitaet
+ZWISCHENFORMAT_AUDIO_FORMAT="wav"               # Audio-Container-Format
+ZWISCHENFORMAT_AUDIO_ENDUNG="wav"               # Dateiendung
+ZWISCHENFORMAT_FORMAT="matroska"                # Container-Format: matroska, mp4
+ZWISCHENFORMAT_ENDUNG="mkv"                     # Dateiendung: mkv, mp4
+
+#------------------------------------------------------------------------------#
+### ZwischenFormat: x264 + PCM64 + MPEG-TS
+
+# Dieses Format musste eingefuehrt werden, weil in Zukunft auch native
+# MP4-Dateien erzeugt werden sollen.
+# Leider kann das Programm MP4Box MKV nicht lesen, deshalb wurde ein
+# Container-Format gesucht, welches moeglichst flexibel und kompatibel ist.
+# MPEG-TS wird von den MKV- und den MP4-Tools gelesen, es kann aber leider nicht
+# mit dem verlusstfreien Video-Codec FFv1 umgehen.
+# x264 mit der Option "-crf 0" ist fast verlusstfrei.
+# Leider ist "x264 -crf 0" aber wesentlich langsamer als FFv1.
+
+#ZWISCHENFORMAT_VIDEO-CODEC="libx264 -crf 0"     # AVC in fast verlusstfreier
+#ZWISCHENFORMAT_AUDIO-CODEC="pcm_f64le"          # beste PCM-Qualitaet
+#ZWISCHENFORMAT_ENDUNG="m2t"                     # mpeg2ts, m2ts, m2t, mts, ts
+
+#
+# Dieses Vorgehen hat die ganze Sache so sehr verkompliziert, dass es
+# verworfen wurde.
+#
+# Ursache fÃuer das Scheitern ist das Programm mp4box.
+# Da auf das Programm mp4box, zum erzeugen der MP4-Dateien, verzichtet werden
+# musste, wurde eine universelle Variante auf Basis von FFmpeg umgesetzt.
+#
+
+#------------------------------------------------------------------------------#
 
 AUFRUF="${0} ${@}"
 
